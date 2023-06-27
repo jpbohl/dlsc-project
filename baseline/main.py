b3d1542b8577bfcec71e5cae7765c8bfce4e0487
Jan Philipp Bohl <jpbohl@protonmail.com>
2023-06-27 16:53:31 +0200

    moved problems to own file

--- baseline/main.py
+++ baseline/main.py
@@ -3,139 +3,7 @@ from torch.utils.data import TensorDataset, DataLoader
 from torch.optim.optimizer import ADAM
 
 from fbpinn import FBPinn
-
-class Cos1d(object):
-
-#define problem together with exact solution to
-#du/dx = cos(ω*x)
-#u(x)=0
-#solution u(x)=1/ω sin(ω*x)
-
-    def __init__(self, domain, nsamples, w):
-
-        self.domain = domain
-        self.nsamples = nsamples
-        self.w = w
-
-    def assemble_dataset(self):
-        """
-        Sample points in given domain and create dataloader
-        for training.
-        """
-
-        sobol = torch.quasirandom.SobolEngine(1, seed=0)
-        points = sobol.draw(self.nsamples)
-
-        points = points * (self.domain[1] - self.domain[0]) - self.domain[0] #caro: ich glaube es sollte + self.domain[0] sein?
-        dataset = TensorDataset(points)
-        dataloader = DataLoader(dataset, batch_size=self.nsamples, shuffle=False)
-
-        return dataloader
-
-    def hard_constraint(self, pred):
-
-        return torch.tanh(self.w * pred) * pred #caro: sollte es nicht tanh(w*input)*pred sein?
-
-    def compute_loss(self, pred, input): #caro: then maybe rename to compute_pde_residual
-        """
-        Compute PDE loss using autograd
-        """
-        pred = self.hard_constraint(pred)
-        dx = torch.autograd.grad(pred.sum(), input, retain_graph=True)
-        f = torch.cos(self.w * input)
-        
-        assert (dx - f).size() == self.nsamples
-
-        return dx - f
-
-    #caro: add function for norm of residual which is to minimize
-
-    def exact_solution(self, input):
-
-        return torch.cos(self.w * input) #caro: exact solution is 1/ω sin(ω*x)
-    
-
-
-
-class Cos1d_multicscale(object):
-
-#define problem together with exact solution to
-#du/dx = ω1 cos(ω1*x) + ω2 cos(ω2*x)
-#u(x)=0
-#solution u(x)=sin(ω1*x)+sin(ω2*x)
-
-
-
-    def __init__(self, domain, nsamples, w):
-
-        self.domain = domain
-        self.nsamples = nsamples
-
-        #in this case w=(w1,w2)
-        self.w1 = w[0]
-        self.w2 = w[1]
-
-        self.training_dataset = self.assemble_dataset()
-
-    def assemble_dataset(self):
-        """
-        Sample points in given domain and create dataloader
-        for training.
-        """
-
-        sobol = torch.quasirandom.SobolEngine(dimension=1, seed=0)
-        points = sobol.draw(self.nsamples)
-
-        #sample points in [a,b]
-        points = points * (self.domain[1] - self.domain[0]) + self.domain[0] #maybe - ?
-        dataset = TensorDataset(points)
-        dataloader = DataLoader(dataset, batch_size=self.nsamples, shuffle=False)
-
-        return dataloader
-
-    def hard_constraint(self, pred, input):
-        """
-        define hard constraint which automatically enforces
-        boundary conditions
-        """
-
-        return torch.tanh(self.w1 * input)*torch.tanh(self.w2 * x) * pred # Isi:  tanh(w1*input)*tanh(w2*input)*pred statt 'x'
-
-    def compute_pde_residual(self, pred, input):
-        """
-        Compute PDE loss using autograd
-        """
-
-
-        pred = self.hard_constraint(pred, input)
-        dx = torch.autograd.grad(pred.sum(), input, retain_graph=True)
-        f = self.w1 * torch.cos(self.w1 * input)+ self.w2 * torch.cos(self.w2 * input)
-        
-        assert (dx - f).size() == self.nsamples
-
-        return dx - f
-    
-    def compute_loss(self, input, verbose=True):
-        """
-        Compute loss by applying the norm to the pde residual 
-        """
-
-
-        #unsupervised
-        r_int  = self.compute_pde_residual(pred, input)
-        loss_int = torch.mean(abs(r_int) ** 2)
-
-        #get log loss 
-        loss = torch.log10(loss_int)
-
-        if verbose: print("Total loss: ", round(loss.item(), 4))
-
-        return loss
-
-
-    def exact_solution(self, input):
-
-        return torch.sin(self.w1 * input)+torch.sin(self.w2 * input)
+from problems import Cos1d, Cos1dMulticscale
 
 
 # define parameters
@@ -154,10 +22,7 @@ lr = 0.0001
 hidden = 2
 neurons = 12 
 
-
-
 problem = Cos1d(domain, nsamples, w = 15)
-
 
 ### Task Caro
 
---
+++ baseline/problems.py
@@ -0,0 +1,148 @@
+import torch
+from torch.utils.data import DataLoader, TensorDataset
+
+
+class Cos1d(object):
+
+#define problem together with exact solution to
+#du/dx = cos(ω*x)
+#u(x)=0
+#solution u(x)=1/ω sin(ω*x)
+
+    def __init__(self, domain, nsamples, w):
+
+        self.domain = domain
+        self.nsamples = nsamples
+        self.w = w
+
+    def assemble_dataset(self):
+        """
+        Sample points in given domain and create dataloader
+        for training.
+        """
+
+        sobol = torch.quasirandom.SobolEngine(1, seed=0)
+        points = sobol.draw(self.nsamples)
+
+        points = points * (self.domain[1] - self.domain[0]) - self.domain[0] #caro: ich glaube es sollte + self.domain[0] sein?
+        dataset = TensorDataset(points)
+        dataloader = DataLoader(dataset, batch_size=self.nsamples, shuffle=False)
+
+        return dataloader
+
+    def hard_constraint(self, pred, input):
+
+        return torch.tanh(self.w * input) * pred 
+
+    def compute_pde_residual(self, pred, input): 
+        """
+        Compute PDE loss using autograd
+        """
+        pred = self.hard_constraint(pred, input)
+        dx = torch.autograd.grad(pred.sum(), input, retain_graph=True)
+        f = torch.cos(self.w * input)
+        
+        assert (dx - f).size() == self.nsamples
+
+        return dx - f
+    
+    def compute_loss(self, pred, input, verbose=True):
+        """
+        Compute loss by applying the norm to the pde residual 
+        """
+        
+        r_int  = self.compute_pde_residual(pred, input)
+        loss_int = torch.mean(abs(r_int) ** 2)
+
+        #get log loss 
+        loss = torch.log10(loss_int)
+
+        if verbose: print("Total loss: ", round(loss.item(), 4))
+
+        return loss
+
+    def exact_solution(self, input):
+
+        return torch.sin(self.w * input) / self.w
+    
+
+
+
+class Cos1dMulticscale(object):
+
+#define problem together with exact solution to
+#du/dx = ω1 cos(ω1*x) + ω2 cos(ω2*x)
+#u(x)=0
+#solution u(x)=sin(ω1*x)+sin(ω2*x)
+
+
+
+    def __init__(self, domain, nsamples, w):
+
+        self.domain = domain
+        self.nsamples = nsamples
+
+        #in this case w=(w1,w2)
+        self.w1 = w[0]
+        self.w2 = w[1]
+
+        self.training_dataset = self.assemble_dataset()
+
+    def assemble_dataset(self):
+        """
+        Sample points in given domain and create dataloader
+        for training.
+        """
+
+        sobol = torch.quasirandom.SobolEngine(dimension=1, seed=0)
+        points = sobol.draw(self.nsamples)
+
+        #sample points in [a,b]
+        points = points * (self.domain[1] - self.domain[0]) + self.domain[0] #maybe - ?
+        dataset = TensorDataset(points)
+        dataloader = DataLoader(dataset, batch_size=self.nsamples, shuffle=False)
+
+        return dataloader
+
+    def hard_constraint(self, pred, input):
+        """
+        define hard constraint which automatically enforces
+        boundary conditions
+        """
+
+        return torch.tanh(self.w1 * input)*torch.tanh(self.w2 * input) * pred 
+
+    def compute_pde_residual(self, pred, input):
+        """
+        Compute PDE loss using autograd
+        """
+
+        pred = self.hard_constraint(pred, input)
+        dx = torch.autograd.grad(pred.sum(), input, retain_graph=True)
+        f = self.w1 * torch.cos(self.w1 * input)+ self.w2 * torch.cos(self.w2 * input)
+        
+        assert (dx - f).size() == self.nsamples
+
+        return dx - f
+    
+    def compute_loss(self, pred, input, verbose=True):
+        """
+        Compute loss by applying the norm to the pde residual 
+        """
+
+
+        #unsupervised
+        r_int  = self.compute_pde_residual(pred, input)
+        loss_int = torch.mean(abs(r_int) ** 2)
+
+        #get log loss 
+        loss = torch.log10(loss_int)
+
+        if verbose: print("Total loss: ", round(loss.item(), 4))
+
+        return loss
+
+
+    def exact_solution(self, input):
+
+        return torch.sin(self.w1 * input)+torch.sin(self.w2 * input)
